ea982865502dd332185c4ab6570f8a45ece819be
pol.bachelin@epitech.eu <pol.bachelin@epitech.eu>
2020-05-10 19:46:48 +0200

    fix():merge conflicts

---
+++ src/parsing/check_label_char.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2020
+** CPE_corewar_2019
+** File description:
+** check_label_char
+*/
+
+#include "assembler.h"
+
+int check_label_chars(char *name)
+{
+    int len_chars = my_strlen(LABEL_CHARS);
+    int x = 0;
+
+    for (int i = 0; name[i]; i++) {
+        x = 0;
+        for (; x < len_chars; x++) {
+            if (name[i] == LABEL_CHARS[x])
+                break;
+        }
+        if (x == len_chars)
+            return 1;
+    }
+    return 0;
+}
--- src/parsing/file_to_struct.c
+++ src/parsing/file_to_struct.c
@@ -96,6 +96,7 @@ int create_command(char **str, funct_t *func, int start_pos)
     else
         len = count_cmd_len(str, start_pos);
     func->name = get_func_name(str[start_pos]);
+    //if (func->name && )
     func->nb_cmd = len;
     func->commands = malloc(sizeof(cmd_t) * (len + 1));
     init_cmd_struct(func->commands, len);

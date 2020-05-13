e92bb43643a0f6d3603640fbdef60ed01b00323d
Ewen <ewen.thomas@epitech.eu>
2020-05-13 14:44:34 +0200

    add funct champ_alive

--- src/live.c
+++ src/live.c
@@ -7,17 +7,17 @@
 
 #include
 
-static int do_live(champ_t *champion, vm_t *vm)
+static int champ_alive(champ_t *champion, vm_t *vm)
 {
     char *str;
 
     vm->cycle_to_die = -1;
     vm->nb_live++;
-//    vm->winner = champion;
+    vm->current_champ = champion;
 //    write(1, "The Champion ", 10);
     str = champion->file_name;
     while (*str != '.' && *str)
-        my_printf(2, "The Champion %s is alive\n", str); // avant le %s y'a un truc ??
+        my_printf(2, "The Champion %s is alive\n", str); //avant le %s y'a un truc ??
 //        write(1, str++, 1);
 //    write(1, " is alive.\n", 11);
     return 0;

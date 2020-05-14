7f24648e0440b38c76ca9d1bc3a790b4b2e8fbd0
simon.guyader@epitech.eu <simon.guyader@epitech.eu>
2020-05-14 17:44:46 +0200

    follow up end

--- asm/src/assembler/create_cor_file.c
+++ asm/src/assembler/create_cor_file.c
@@ -20,5 +20,7 @@ void create_cor_file(champion_header_t *champ, char const *file_name)
     champ->file_name = my_strcat(champ->file_name, ".cor");
     champ->fd = open(champ->file_name, O_WRONLY | O_CREAT | O_TRUNC,
                      S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (champ->fd == -1)
+        exit(EXIT_FAILURE);
     free(name);
 }

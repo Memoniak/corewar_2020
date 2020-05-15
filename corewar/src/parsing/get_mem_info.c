52562ce6ddf5fb76312ec5eacf3baa205ff54ffb
evan.sabre@epitech.eu <evan.sabre@epitech.eu>
2020-05-15 15:25:09 +0200

    fix error about get_mem_info.c

--- corewar/src/parsing/get_mem_info.c
+++ corewar/src/parsing/get_mem_info.c
@@ -15,17 +15,17 @@ int get_param_type(vm_t *vm, process_t *proc, int nb)
 
     if (!is_typed(code - 1))
     {
-        params[0] = check_direct(OTM(code - 1));
-        return params[nb - 1];
+	params[0] = check_direct(OTM(code - 1));
+	return params[nb - 1];
     }
     for (ssize_t i = 0; i != 4; i++)
     {
-        temp = vm->mem[proc->pc + 1] >> (2 * i);
-        temp &= 0x03;
-        if (temp == 2)
-            params[3 - i] = check_direct(OTM(code - 1));
-        else
-            params[3 - i] = temp;
+	temp = vm->mem[proc->pc + 1] >> (2 * i);
+	temp &= 0x03;
+	if (temp == 2)
+	    params[3 - i] = check_direct(OTM(code - 1));
+	else
+	    params[3 - i] = temp;
     }
     return params[nb - 1];
 }
@@ -36,8 +36,8 @@ static int move(vm_t *vm, process_t *proc, int nb)
 
     for (ssize_t i = 1; i != nb; i++)
     {
-        temp += get_param_type(vm, proc, i);
-        my_printf(2, "%d ", temp);
+	temp += get_param_type(vm, proc, i);
+	my_printf(2, "%d ", temp);
     }
     return (proc->pc + is_typed(vm->mem[proc->pc] - 1) + temp);
 }
@@ -50,13 +50,6 @@ int get_param_value(vm_t *vm, process_t *proc, int nb)
     int result = 0;
 
     result = read_nbytes(&next, byte, code);
-    if (byte == 1)
-        my_printf(2, SRED, "REG ");
-    else if (byte == 2)
-        my_printf(2, SRED, "DIR ");
-    else
-        my_printf(2, SRED, "IND ");
-    result = read_nbytes(&next, byte, code);
     return result;
 }
 
@@ -67,9 +60,9 @@ int get_next_pc(vm_t *vm, process_t *proc)
 
     for (ssize_t i = 1; i != 5; i++)
     {
-        result += get_param_type(vm, proc, i);
+	result += get_param_type(vm, proc, i);
     }
     if (!is_typed(code - 1))
-        return result + 1;
+	return result + 1;
     return result + 2;
 }
